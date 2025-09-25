5d7d097a4e0dd0b175bb2cac1dec4c14380b134c
sbouiii <96526303+sbouiii@users.noreply.github.com>
2025-09-25 21:36:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          barra mrigl
+          barra mrigl llllllll
         </p>
         <a
           className="App-link"
