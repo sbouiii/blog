6d28db391e6a34e63d2e9f1a3e6ebe0520be9b74
sbouiii <96526303+sbouiii@users.noreply.github.com>
2025-10-02 19:34:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-webhook success  sdd44d </p>
+CI / CD </p>
         <a
           className="App-link"
           href="https://reactjs.org"
