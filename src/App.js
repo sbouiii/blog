646d7554ca99ef4c28093a5c04f44045cd090082
sbouiii <96526303+sbouiii@users.noreply.github.com>
2025-11-04 19:00:19 +0100

    Update text from 'CI / CD' to 'good job'

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-CI / CD </p>
+good job </p>
         <a
           className="App-link"
           href="https://reactjs.org"
